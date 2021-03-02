8a5796b1a6f53d201e0e42fe3b62c6279bf7ddf2
Aneta <72556989+anetaap@users.noreply.github.com>
2021-03-02 20:45:56 +0100

    Update zaj1.cpp

--- zaj1.cpp
+++ zaj1.cpp
@@ -1,3 +1,26 @@
 //Proszę o napisanie funkcji doubleCopy przyjmującej tablice liczb (w postaci wskaźników od-do) i zwracającą kopię (alokacja przy pomocy new) tablicy z podwojonymi wartościami
 //proszę pamiętać aby sprawdzić czy wskaźniki nie są null
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+void doubleCopy(int *a, int n){
+    
+    int * tablica2 = new int[n];
+    for(int i=0;i<n;i++){
+      tablica2[i]=*(a + i) *2;
+    }
+    for (int i=0; i<n; i++){
+           cout << tablica2[i] << " ";
+    }
+   delete [] tablica2;
+}
+int main()
+{
+    int tab[5] = {1,2,3,4,5}; 
+    int *ptab;
+    doubleCopy(tab,5);
+    
+    return 0;
+} 
