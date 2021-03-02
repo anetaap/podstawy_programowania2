8691922a5682af047382059b4435bb68f07f04f0
Aneta <72556989+anetaap@users.noreply.github.com>
2021-03-02 20:11:44 +0100

    Create zaj1.cpp

---
+++ zaj1.cpp
@@ -0,0 +1,3 @@
+//Proszę o napisanie funkcji doubleCopy przyjmującej tablice liczb (w postaci wskaźników od-do) i zwracającą kopię (alokacja przy pomocy new) tablicy z podwojonymi wartościami
+//proszę pamiętać aby sprawdzić czy wskaźniki nie są null
+
