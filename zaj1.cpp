8513cfe9a16c79151b8c7dd767a77cc152de23a9
Aneta <72556989+anetaap@users.noreply.github.com>
2021-03-02 20:55:25 +0100

    Update zaj1.cpp

--- zaj1.cpp
+++ zaj1.cpp
@@ -11,16 +11,22 @@ void doubleCopy(int *a, int n){
     for(int i=0;i<n;i++){
       tablica2[i]=*(a + i) *2;
     }
+    cout <<"Tablica druga: " << endl; 
     for (int i=0; i<n; i++){
-           cout << tablica2[i] << " ";
+           cout << tablica2[i] << "";
     }
    delete [] tablica2;
 }
 int main()
 {
-    int tab[5] = {1,2,3,4,5}; 
-    int *ptab;
+    int tab[5] = {1,2,3,4,5};
+    cout <<"Tablica pierwsza: " << endl; 
+    for (int i=0; i<5; i++){
+        cout << tab[i] << "";
+    }
+    cout << endl;
     doubleCopy(tab,5);
     
     return 0;
 } 
+ 
