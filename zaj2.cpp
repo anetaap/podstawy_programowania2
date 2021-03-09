4a04d7ead3fcc94f0d6d265c4b6612bbe039f9c9
Aneta <72556989+anetaap@users.noreply.github.com>
2021-03-09 20:46:32 +0100

    Update zaj2.cpp

--- zaj2.cpp
+++ zaj2.cpp
@@ -34,6 +34,52 @@ public :
     double perimeter() const{return 2*side1+2*side2;}
 };
 
+//3. Proszę o zrobienie gadających (wypisujących na cout) konstruktorów dla zadanych klas
+class Rectangle2
+{
+private :
+    int side1, side2;
+public:
+    Rectangle2(int a){
+      side1=a;
+      cout<<"Kwadrat\n";
+    }
+    Rectangle2(int a, int b){
+      side1=a;
+      side2=b;
+      cout<<"Prostokąt\n";
+    }
+};
+
+class Triangle
+{
+private:
+    int side1,side2,side3;
+public:
+    Triangle(int a){ 
+      side1=a;
+      cout<<"Trójąkt równoboczny\n"; 
+        }
+    Triangle(int a, int b, int c){
+       side1=a;
+       side2=b;
+       side3=c;
+       cout<<"Trójąt różnoboczny\n";   
+    }
+};
+
+class Circle2
+{
+private:
+    int radius;
+public:
+    Circle2(int r=3){
+      radius=r;
+      cout<<"Koło\n";
+    }
+};
+
+
 int main(){
   Circle Kolo(3);
   cout<<"Pole koła wynosi: "<<Kolo.area()<<endl;
@@ -42,5 +88,9 @@ int main(){
   Rectangle Rec(3,4);
   cout<<"Pole prostokąta jest równe: "<<Rec.area()<<endl;
   cout<<"Obwód prostokąta to: "<<Rec.perimeter()<<endl;
+  Rectangle2 Rec2(3,4);
+  Triangle troj(3);
+  Triangle troj2(2,4,5);
+  Circle2 sth;
   return 0;
 }
