5c28b25d860973e024427ce104ae15b32bd29e91
Aneta <72556989+anetaap@users.noreply.github.com>
2021-05-11 18:12:45 +0200

    Create zaj10.cpp

---
+++ zaj10.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class String
+{
+    char* text;
+    size_t size_;
+public:
+    String(){
+    size_ = 0;
+    text = new char[0];
+    }
+    String(const char* txt){
+        if (txt == nullptr){ throw length_error("The text does not exist.");}
+        else{
+            size_ = strlen(txt);
+            text = new char[size_ + 1]{};
+            cout << size_ << " size" << endl;
+            copy(txt, txt + size_, text);
+            cout << text << endl;
+        }
+    }
+    String(String& org){
+    size_ = org.size_;
+    text = new char[size_];
+    copy(org.text, org.text + org.size_, text);
+    }
+    ~String(){
+        if (text != nullptr)
+        delete text;
+    }
+    size_t size1(){
+        return size_;
+    }
+    char* text_(){
+        return text;
+    }
+};
+
+
+int main()
+{
+    String A("Fear is the mind-killer.");
+    cout << A.text_() << endl;
+    return 0;
+}
