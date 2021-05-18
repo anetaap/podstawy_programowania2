f9c737a1f709fc006379d6d24876c296731a46a6
Aneta <72556989+anetaap@users.noreply.github.com>
2021-05-18 15:10:52 +0200

    Update zaj10.cpp

--- zaj10.cpp
+++ zaj10.cpp
@@ -7,10 +7,13 @@ class String
     char* text;
     size_t size_;
 public:
+
+//A) Konstruktor bezargumentowy
     String(){
     size_ = 0;
     text = new char[0];
     }
+// B) Konstruktor przyjmujący tekst
     String(const char* txt){
         if (txt == nullptr){ throw length_error("The text does not exist.");}
         else{
@@ -21,35 +24,58 @@ public:
             cout << text << endl;
         }
     }
+// D) Konstruktor kopiujący
     String(String& org){
-    size_ = org.size_;
-    text = new char[size_];
-    copy(org.text, org.text + org.size_, text);
+        size_ = org.size_;
+        text = new char[size_];
+        copy(org.text, org.text + org.size_ + 1, text);
+        cout<<"Głęboka kopia: "<<text<<endl;
     }
+// C) Destruktor zwalniający pamięć
     ~String(){
-        if (text != nullptr)
-        delete text;
+        delete []text;
     }
-    void operator=(const char* newText){
+//  F) Proszę o zdefiniowanie operatora=(const char* newText)
+    String& operator=(const char* newText){
         if (newText == nullptr){ throw length_error("The text does not exist.");}
         else{
             size_ = sizeof(newText)/sizeof(char*);
             text = new char[size_];
-            copy(newText, newText + size_, text);
+            cout<<"operator"<<endl;
+            copy(newText, newText + size_ + 1, text);
         }
     }
-    size_t size1(){
+//   G) size() i text()
+    size_t size1() const{
         return size_;
     }
-    char* text_(){
+    char* text_() const{
         return text;
     }
+//  H) operator=(const String& newText)
+    String & operator=(const String &NewText){
+        if (this==&NewText)
+            return *this;
+
+        if (NewText.text == nullptr){ throw length_error("The text does not exist.");}
+
+        text = new char[NewText.size_];
+        copy(NewText.text, NewText.text + NewText.size_ + 1, text);
+        size_ = NewText.size_;
+        return *this;
+    }
+
 };
 
 
 int main()
 {
     String A("Fear is the mind-killer.");
-    cout << A.text_() << endl;
+    String B = A;
+    cout <<"Kopia "<< B.text_() << endl;
+    String s, s2, s3("Diuna");
+    s=s2=s3;
+    s3 = s3;
+    cout<<"test: "<<s.text_() <<","<< s2.text_()<<","<<s3.text_()<<endl;
     return 0;
 }
